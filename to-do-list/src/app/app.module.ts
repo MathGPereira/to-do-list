b73e2a4e5c083a414800f93fd9bfd4505fa641e2
Matheus Pereira <matheusgp.mto@outlook.com>
2023-05-21 21:17:39 -0300

    Criando a estrutura do componente de atividade

--- to-do-list/src/app/app.module.ts
+++ to-do-list/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './componentes/formulario/formulario.component';
+import { AtividadeComponent } from './componentes/atividades/atividade/atividade.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent
+    FormularioComponent,
+    AtividadeComponent
   ],
   imports: [
     BrowserModule,
